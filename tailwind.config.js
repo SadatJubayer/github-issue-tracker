b3d935dcb59b375845923fc8d5e5c9c73915c66c
SadatJubayer <sadatjubayer@gmail.com>
2022-04-09 14:24:04 +0600

    📦 NEW: Theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#9A41EA',
+        secondary: '#E6E7E9',
+        error: '#FF0000',
+        success: '#0DD9A8',
+        background: '#040C28',
+        hover: '#1D243D',
+        blue: '#3267F0',
+      },
+    },
   },
   plugins: [],
 };
