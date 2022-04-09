75063eab7e38a2a47ed91c816e7fc4bbb2d421f6
SadatJubayer <sadatjubayer@gmail.com>
2022-04-10 00:05:49 +0600

    👌 IMPROVE: env added

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,12 +1,11 @@
 import Axios from 'axios';
 
-const token = '';
 const baseURL = 'https://api.github.com';
 
 export const axios = Axios.create({
     baseURL,
     headers: {
         Accept: 'application/json',
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
     },
 });
