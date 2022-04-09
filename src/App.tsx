4f9d602052ad2685f85565115073900245ec515a
SadatJubayer <sadatjubayer@gmail.com>
2022-04-09 14:33:55 +0600

    📦 NEW: ESLint, Prettier & Absolute import

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Dummy } from 'components/Dummy';
+
 const App = () => {
-  return (
-    <div className="bg-gray-100">
-      <h1 className="text-5xl text-center py-5">Hello world</h1>
-    </div>
-  );
+    return (
+        <div className="bg-background">
+            <Dummy />
+            <h1 className="text-5xl text-center text-primary py-5">Hello world</h1>
+        </div>
+    );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,10 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
