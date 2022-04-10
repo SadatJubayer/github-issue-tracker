ae0bf8c035c6b2feb7a425ab3e7c15a216782d39
SadatJubayer <sadatjubayer@gmail.com>
2022-04-10 15:02:13 +0600

    👌 IMPROVE: FontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            },
             colors: {
                 primary: '#9A41EA',
                 secondary: '#E6E7E9',
